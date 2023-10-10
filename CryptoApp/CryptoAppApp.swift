1ed3851083a8ee347cf16ac8681b8b1cf6c513b6
Joaquin Castrillon <62633394+joaqcn@users.noreply.github.com>
2023-10-09 22:59:14 -0400

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Joaquin Castrillon on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
